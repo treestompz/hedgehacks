4471cef227f291ec6a5577bf299eed3467907ff5
Stefan Gorski <sforstefan@hotmail.com>
2024-04-14 13:52:45 -0400

    change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import './css/lib/bootstrap/bootstrap-cyborg.css'
 
 export const metadata: Metadata = {
-  title: 'hedgehacks',
+  title: 'hedgehacks | GTrend Tool',
   description:
     'Hedgehacks is a GTrend social arb trading tool to help you find your next big trade or idea.',
 }
